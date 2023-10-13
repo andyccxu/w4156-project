83160acdc0537e5d9ae7dacc1d6ac03f53abb937
Serdar Mamadzhanov <sitior@me.com>
2023-10-13 15:18:05 -0400

    added config folder & a model file for facilities

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # OptiStaff API
-# Smart Dynamic Scheduler (Schedule Builder)
+
+Smart Dynamic Scheduler (Schedule Builder)
 
 Install packages:
-`npm install express cors body-parser mongodb mongoose`
+`npm i express mongoose dotenv cors bcryptjs morgan jsonwebtoken`
+Explanation:
+express — To manage the server and routes.
+mongoose — To connect with MongoDB
+dotenv — To load environment variables from the .env file
+cors — To allow api access from different domains
+bcryptjs — To hash passwords
+morgan — To log HTTP requests and errors
+jsonwebtoken — for authentication
 
 To run the server:
-node index.js
+`node index.js`
 
 To stop the server:
-ctrl + c
+`ctrl + c`
+
+To test the API, install: <https://www.postman.com>
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require('express')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 const app = express();
-const port = 3000;
+
+
+// Load config file
+dotenv.config({ path: './config/config.env' })
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
-// connect to mongodb
-const uri = "mongodb+srv://fourloop:fourloop_thebest@cluster0.o08w0ip.mongodb.net/?retryWrites=true&w=majority"
-
+const uri = process.env.MONGO_URI;
 async function connect() {
   try {
-    await mongoose.connect(uri);
+    await mongoose.connect(uri)
     console.log("Connected to MongoDB");
   } catch (err) {
     console.error(err);
@@ -20,7 +22,7 @@ async function connect() {
 }
 
 connect();
-
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
