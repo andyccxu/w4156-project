94fe87b7f47fd3a04a9ba8e1d04b10a233c81087
Andy Xu <andyccxu@outlook.com>
2023-11-17 20:14:57 -0500

    refactor controller functions for schedules into /controllers

---
+++ controllers/scheduleController.js
@@ -0,0 +1,202 @@
+const moment = require('moment');
+
+const scheduling = require('../service/scheduling');
+const {Schedule, ScheduleEntry} = require('../models/Schedule');
+const Staff = require('../models/Staff');
+const Facility = require('../models/Facility');
+
+/**
+ * Controller for route GET /schedules
+ *
+ * @param {*} req Express.js request object
+ * @param {*} res Express.js response object
+ */
+async function getAllController(req, res) {
+  try {
+    const schedules = await Schedule.find();
+    res.json(schedules);
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+}
+
+/**
+ * Controller for route GET /schedules/{:id}
+ *
+ * @param {*} req Express.js request object
+ * @param {*} res Express.js response object
+ */
+function getOneController(req, res) {
+  res.json({
+    sid: req.params.id,
+    schedule: res.schedule,
+  });
+}
+
+/**
+ * Controller for route CREATE /schedules
+ * @param {*} req Express.js request object
+ * @param {*} res Express.js response object
+ */
+async function createController(req, res) {
+  // query the facility by name
+  const fname = req.body.facility;
+  const facility = await Facility.findOne({facilityName: fname});
+  if (!facility) {
+    // cannot find the facility
+    res.status(404).json({message: 'Cannot find the facility: ', fname});
+    return;
+  }
+
+  // get all the staff working at the facility
+  let staffMembers;
+  try {
+    staffMembers = await Staff.find({assignedFacility: facility._id});
+  } catch (err) {
+    res.status(500).json({message: err.message});
+    return;
+  }
+
+  // create a new schedule
+  schedule = new Schedule({
+    facilityId: facility._id,
+  });
+
+  const shifts = scheduling.computeShifts(facility.operatingHours.start,
+      facility.operatingHours.end,
+      facility.numberShifts);
+
+  // create a new schedule entry for each staff
+  for (const staff of staffMembers) {
+    // randomly select a shift
+    const shift = shifts[Math.floor(Math.random() * shifts.length)];
+
+    const scheduleEntry = new ScheduleEntry({
+      staffId: staff._id,
+      start: shift.start,
+      end: shift.end,
+    });
+
+    // we fill the shifts field of the schedule
+    schedule.shifts.push(scheduleEntry);
+  }
+
+
+  try {
+    const newSchedule = await schedule.save();
+    res.status(201).json(newSchedule);
+  } catch (err) {
+    // something wrong with the user input
+    res.status(400).json({message: err.message});
+  }
+}
+
+/**
+ * Controller for route PATCH /schedules/{:id}
+ *
+ * @param {*} req Express.js request object
+ * @param {*} res Express.js response object
+ */
+async function patchController(req, res) {
+  schedule = res.schedule;
+  facility = res.facility;
+
+  // loop over the shifts in the request body
+  for (const shift of req.body.shifts) {
+    // validate the input
+    const instance = new ScheduleEntry({
+      staffId: shift.staffId,
+      start: shift.start,
+      end: shift.end,
+      days: shift.days,
+    });
+
+    try {
+      await instance.validate();
+    } catch (error) {
+      res.status(400).json({
+        error: error.message,
+        message: 'Error: Invalid input format.',
+      });
+      return;
+    }
+
+    // check input time format
+    if (!moment(shift.start, 'HH:mm', true).isValid() ||
+    !moment(shift.end, 'HH:mm', true).isValid()) {
+      res.status(400).json({
+        input_start: shift.start,
+        input_end: shift.end,
+        message: 'Error: Invalid start/end time format. ' +
+        'Strict parsing with format HH:mm.',
+      });
+      return;
+    }
+
+    // check start time < end time
+    if (moment(shift.start, 'HH:mm').isAfter(moment(shift.end, 'HH:mm'))) {
+      res.status(400).json({
+        start: shift.start,
+        end: shift.end,
+        message: 'Error: Start time must be before end time.',
+      });
+      return;
+    }
+
+    // check the facility opening time
+    startValid = scheduling.isOperatingTime(shift.start,
+        facility.operatingHours.start,
+        facility.operatingHours.end);
+    endValid = scheduling.isOperatingTime(shift.end,
+        facility.operatingHours.start,
+        facility.operatingHours.end);
+
+    if (!startValid || !endValid) {
+      res.status(400).json({
+        start: shift.start,
+        end: shift.end,
+        message: 'Error: Start/end time is out of the operating hours.',
+      });
+      return;
+    }
+  };
+
+  // update the shifts property of the schedule
+  try {
+    await Schedule.findOneAndUpdate(
+        {_id: req.params.id},
+        {shifts: req.body.shifts},
+    );
+  } catch (err) {
+    res.status(500).json({message: err.message});
+    return;
+  }
+
+  res.status(200).json({
+    message: 'Success: shifts schedule updated',
+  });
+}
+
+/**
+ * Controller for route DELETE /schedules/{:id}
+ *
+ * @param {*} req Express.js request object
+ * @param {*} res Express.js response object
+ */
+async function deleteController(req, res) {
+  try {
+    await res.schedule.deleteOne();
+    res.json({message: 'Deleted schedule'});
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+}
+
+// export all functions
+module.exports = {
+  getAllController,
+  getOneController,
+  createController,
+  patchController,
+  deleteController,
+};
--- routes/schedules.js
+++ routes/schedules.js
@@ -1,178 +1,29 @@
 const express = require('express');
-const moment = require('moment');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-const {Schedule, ScheduleEntry} = require('../models/Schedule');
+const {Schedule} = require('../models/Schedule');
 const Facility = require('../models/Facility');
-const Staff = require('../models/Staff');
 
-const scheduling = require('../service/scheduling');
+const scheduleController = require('../controllers/scheduleController');
 
 
 // GET all shifting schedules
-router.get('/', async (req, res) => {
-  try {
-    const schedules = await Schedule.find();
-    res.json(schedules);
-  } catch (err) {
-    res.status(500).json({message: err.message});
-  }
-});
+router.get('/', scheduleController.getAllController);
 
 // GET one shifting schedule
-router.get('/:id', getSchedule, (req, res) => {
-  res.json({
-    sid: req.params.id,
-    schedule: res.schedule,
-  });
-});
+router.get('/:id', getSchedule, scheduleController.getOneController);
 
 // POST new schedule for a facility
-router.post('/', async (req, res) => {
-  // query the facility by name
-  const fname = req.body.facility;
-  const facility = await Facility.findOne({facilityName: fname});
-  if (!facility) {
-    // cannot find the facility
-    res.status(404).json({message: 'Cannot find the facility: ', fname});
-    return;
-  }
-
-  // get all the staff working at the facility
-  let staffMembers;
-  try {
-    staffMembers = await Staff.find({assignedFacility: facility._id});
-  } catch (err) {
-    res.status(500).json({message: err.message});
-    return;
-  }
-
-  // create a new schedule
-  schedule = new Schedule({
-    facilityId: facility._id,
-  });
-
-  const shifts = scheduling.computeShifts(facility.operatingHours.start,
-      facility.operatingHours.end,
-      facility.numberShifts);
-
-  // create a new schedule entry for each staff
-  for (const staff of staffMembers) {
-    // randomly select a shift
-    const shift = shifts[Math.floor(Math.random() * shifts.length)];
-
-    const scheduleEntry = new ScheduleEntry({
-      staffId: staff._id,
-      start: shift.start,
-      end: shift.end,
-    });
-
-    // we fill the shifts field of the schedule
-    schedule.shifts.push(scheduleEntry);
-  }
-
-
-  try {
-    const newSchedule = await schedule.save();
-    res.status(201).json(newSchedule);
-  } catch (err) {
-    // something wrong with the user input
-    res.status(400).json({message: err.message});
-  }
-});
+router.post('/', scheduleController.createController);
 
 // PATCH update a specific schedule entry
-router.patch('/:id', getSchedule, getFacility, async (req, res) => {
-  schedule = res.schedule;
-  facility = res.facility;
-
-  // loop over the shifts in the request body
-  for (const shift of req.body.shifts) {
-    // validate the input
-    const instance = new ScheduleEntry({
-      staffId: shift.staffId,
-      start: shift.start,
-      end: shift.end,
-      days: shift.days,
-    });
-
-    try {
-      await instance.validate();
-    } catch (error) {
-      res.status(400).json({
-        error: error.message,
-        message: 'Error: Invalid input format.',
-      });
-      return;
-    }
-
-    // check input time format
-    if (!moment(shift.start, 'HH:mm', true).isValid() ||
-    !moment(shift.end, 'HH:mm', true).isValid()) {
-      res.status(400).json({
-        input_start: shift.start,
-        input_end: shift.end,
-        message: 'Error: Invalid start/end time format. ' +
-        'Strict parsing with format HH:mm.',
-      });
-      return;
-    }
-
-    // check start time < end time
-    if (moment(shift.start, 'HH:mm').isAfter(moment(shift.end, 'HH:mm'))) {
-      res.status(400).json({
-        start: shift.start,
-        end: shift.end,
-        message: 'Error: Start time must be before end time.',
-      });
-      return;
-    }
-
-    // check the facility opening time
-    startValid = scheduling.isOperatingTime(shift.start,
-        facility.operatingHours.start,
-        facility.operatingHours.end);
-    endValid = scheduling.isOperatingTime(shift.end,
-        facility.operatingHours.start,
-        facility.operatingHours.end);
-
-    if (!startValid || !endValid) {
-      res.status(400).json({
-        start: shift.start,
-        end: shift.end,
-        message: 'Error: Start/end time is out of the operating hours.',
-      });
-      return;
-    }
-  };
-
-  // update the shifts property of the schedule
-  try {
-    await Schedule.findOneAndUpdate(
-        {_id: req.params.id},
-        {shifts: req.body.shifts},
-    );
-  } catch (err) {
-    res.status(500).json({message: err.message});
-    return;
-  }
-
-  res.status(200).json({
-    message: 'Success: shifts schedule updated',
-  });
-});
+router.patch('/:id', getSchedule, getFacility,
+    scheduleController.patchController);
 
 // DELETE one schedule
-router.delete('/:id', getSchedule, async (req, res) => {
-  try {
-    await res.schedule.deleteOne();
-    res.json({message: 'Deleted schedule'});
-  } catch (err) {
-    res.status(500).json({message: err.message});
-  }
-});
+router.delete('/:id', getSchedule, scheduleController.deleteController);
 
 
 /**
