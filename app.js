502f454bfa96711253936c9f6d947cf1281e3289
Andy Xu <andyccxu@outlook.com>
2024-04-06 21:57:43 -0400

    Add .prettierrc

--- app.js
+++ app.js
@@ -4,15 +4,13 @@ const verifyAuth = require('./middlewares/verifyAuth');
 
 const dotenv = require('dotenv');
 // Load config file
-dotenv.config({path: './config/config.env'});
-
+dotenv.config({ path: './config/config.env' });
 
 const app = express();
 // We want express to parse user input json
 app.use(express.json());
 // use cors for cross origin resource sharing
 app.use(cors());
-
 
 // Routes
 const authRouter = require('./routes/auth');
@@ -21,16 +19,16 @@ app.use('/auth', authRouter);
 const usersRouter = require('./routes/users');
 app.use('/profile', verifyAuth, usersRouter);
 
-const {router: facilitiesRouter} = require('./routes/facilities');
+const { router: facilitiesRouter } = require('./routes/facilities');
 app.use('/facilities', verifyAuth, facilitiesRouter);
 
-const {router: notificationsRouter} = require('./routes/notifications');
-app.use('/notifications', verifyAuth, notificationsRouter);
+// const { router: notificationsRouter } = require('./routes/notifications');
+// app.use('/notifications', verifyAuth, notificationsRouter);
 
-const {router: schedulesRouter} = require('./routes/schedules');
+const { router: schedulesRouter } = require('./routes/schedules');
 app.use('/schedules', verifyAuth, schedulesRouter);
 
-const {router: employeesRouter} = require('./routes/employees');
+const { router: employeesRouter } = require('./routes/employees');
 app.use('/employees', verifyAuth, employeesRouter);
 
 // export app
